7d360c01f5621150cf2e62661676e725478742fe
vicoguim <vicoguim@gmail.com>
2017-01-30 15:49:42 -0200

    Fixing logout

--- php/logout.php
+++ php/logout.php
@@ -3,14 +3,6 @@
 session_start('tool');
 
 require_once "../conf/general.php";
-require_once INCLUDE_ADODB;
-require_once INCLUDE_ADODB_ERROR;
-
-$conexao = ADONewConnection(DATABASE_DRIVER);
-$conexao->Connect(DATABASE_SERVER, DATABASE_USER, DATABASE_PASSWORD, DATABASE_NAME);
-
-$sSQL = "UPDATE access_log SET logout=current_timestamp where id='".$_SESSION["log_id"]."';";
-$conexao->Execute($sSQL);
 
 unset($_SESSION);
 session_destroy();
