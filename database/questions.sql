efc5b3fc1983a89a4fcc692eee87f790e5731d81
vicoguim <vicoguim@gmail.com>
2018-01-21 23:01:50 -0200

    Adding questions table in the database

---
+++ database/questions.sql
@@ -0,0 +1,34 @@
+
+create sequence questions_seq;
+create table questions (
+    id integer DEFAULT nextval('questions_seq'::regclass) NOT NULL,
+    question text,
+    answer_type smallint DEFAULT 1, -- for future implementations ...
+    created_by integer,
+    active smallint DEFAULT 1
+);
+
+ALTER TABLE public.questions OWNER TO tool;
+ALTER TABLE ONLY questions ADD CONSTRAINT questions_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY questions ADD CONSTRAINT user_fkey FOREIGN KEY (created_by) REFERENCES users (id);
+
+insert into questions (question,created_by) values ('What are the strengths of the paper?',1);
+insert into questions (question,created_by) values ('What are the weaknesses of the paper?',1);
+insert into questions (question,created_by) values ('Did you recommend this paper?',1);
+insert into questions (question,created_by) values ('What are the main topics of the paper?',1);
+
+create sequence papers_users_answers_seq;
+create table papers_users_answers (
+    id          integer NOT NULL DEFAULT nextval('papers_users_answers_seq'),
+    answer      text,
+    time        timestamp   DEFAULT current_timestamp,
+    question_id integer,
+    user_id     integer,    
+    paper_id    integer
+);
+
+ALTER TABLE public.papers_users_answers OWNER TO tool;
+ALTER TABLE ONLY papers_users_answers ADD CONSTRAINT papers_users_answers_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY papers_users_answers ADD CONSTRAINT user_fkey FOREIGN KEY (user_id) REFERENCES users (id);
+ALTER TABLE ONLY papers_users_answers ADD CONSTRAINT paper_fkey FOREIGN KEY (paper_id) REFERENCES papers (id);
+ALTER TABLE ONLY papers_users_answers ADD CONSTRAINT question_fkey FOREIGN KEY (question_id) REFERENCES questions (id);
